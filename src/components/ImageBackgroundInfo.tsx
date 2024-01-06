8848498c73dd1174375bd0b57017e2cd183f972c
ashim1588 <ashimmehar158@gmail.com>
2024-01-06 22:58:49 +0530

    Image Background Component Complete

--- src/components/ImageBackgroundInfo.tsx
+++ src/components/ImageBackgroundInfo.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   ImageBackground,
@@ -9,7 +9,13 @@ import {
   Text,
 } from 'react-native';
 import GradientBGIcon from './GradientBGIcon';
-import {COLORS, FONTSIZE, SPACING} from '../theme/theme';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
 import CustomIcon from './CustomIcon';
 
 interface ImageBackgroundInfoProps {
@@ -98,7 +104,41 @@ const ImageBackgroundInfo: React.FC<ImageBackgroundInfoProps> = ({
                     size={type === 'Bean' ? FONTSIZE.size_18 : FONTSIZE.size_24}
                     color={COLORS.primaryOrangeHex}
                   />
+                  <Text
+                    style={[
+                      styles.PropertyFirstText,
+                      {
+                        marginTop:
+                          type === 'Bean'
+                            ? SPACING.space_4 + SPACING.space_2
+                            : 0,
+                      },
+                    ]}>
+                    {type}
+                  </Text>
                 </View>
+                <View style={styles.PropertyFirst}>
+                  <CustomIcon
+                    name={type === 'Bean' ? 'location' : 'drop'}
+                    size={FONTSIZE.size_16}
+                    color={COLORS.primaryOrangeHex}
+                  />
+                  <Text style={styles.PropertyTextLast}>{ingredients}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.InfoContainerRow}>
+              <View style={styles.RatingContainer}>
+                <CustomIcon
+                  name={'star'}
+                  color={COLORS.primaryOrangeHex}
+                  size={FONTSIZE.size_20}
+                />
+                <Text style={styles.RatingText}>{average_rating}</Text>
+                <Text style={styles.RatingCountText}>({ratings_count})</Text>
+              </View>
+              <View style={styles.RoastedContainer}>
+                <Text style={styles.RoastedText}>{roasted}</Text>
               </View>
             </View>
           </View>
@@ -126,11 +166,84 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
-  ImageInfoOuterContainer: {},
-  ImageInfoInnerContainer: {},
-  InfoContainerRow: {},
-  ItemTitleText: {},
-  ItemSubtitleText: {},
+  ImageInfoOuterContainer: {
+    paddingVertical: SPACING.space_24,
+    paddingHorizontal: SPACING.space_30,
+    backgroundColor: COLORS.secondaryBlackRGBA,
+    borderTopLeftRadius: BORDERRADIUS.radius_20 * 2,
+    borderTopRightRadius: BORDERRADIUS.radius_20 * 2,
+  },
+  ImageInfoInnerContainer: {
+    justifyContent: 'space-between',
+    gap: SPACING.space_15,
+  },
+  InfoContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  ItemTitleText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.primaryWhiteHex,
+  },
+  ItemSubtitleText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  ItemPropertiesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.space_20,
+  },
+  PropertyFirst: {
+    height: 55,
+    width: 55,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  PropertyFirstText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+  },
+  PropertyTextLast: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+    marginTop: SPACING.space_2 + SPACING.space_4,
+  },
+  RatingContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_10,
+    alignItems: 'center',
+  },
+  RatingText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+  RatingCountText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  RoastedContainer: {
+    height: 55,
+    width: 55 * 2 + SPACING.space_20,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  RoastedText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+  },
 });
 
 export default ImageBackgroundInfo;
