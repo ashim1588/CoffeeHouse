88d5115d4f163e2580539da730fdb6c35441a01e
ashim1588 <ashimmehar158@gmail.com>
2024-01-15 15:42:02 +0530

    DecrementCartItemQuantity Logic

--- src/store/store.ts
+++ src/store/store.ts
@@ -143,6 +143,33 @@ export const useStore = create(
             }
           }),
         ),
+      decrementCartItemQuantity: (id: string, size: string) =>
+        set(
+          produce(state => {
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id === id) {
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (state.CartList[i].prices[j].size === size) {
+                    if (state.CartList[i].prices.length > 1) {
+                      if (state.CartList[i].prices[j].quantity > 1) {
+                        state.CartList[i].prices[j].quantity--;
+                      } else {
+                        state.CartList[i].prices.splice(j, 1);
+                      }
+                    } else {
+                      if (state.CartList[i].prices[j].quantity > 1) {
+                        state.CartList[i].prices[j].quantity--;
+                      } else {
+                        state.CartList.splice(i, 1);
+                      }
+                    }
+                    break;
+                  }
+                }
+              }
+            }
+          }),
+        ),
     }),
     {
       name: 'coffe-app',
