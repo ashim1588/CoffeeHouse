f866991fc71074a29ef2e14667d13ddc11e1d504
ashim1588 <ashimmehar158@gmail.com>
2024-01-17 17:22:21 +0530

    Empty List and Animation and Header in Cart Screen

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,17 +1,60 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, View} from 'react-native';
 import React from 'react';
 import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {COLORS} from '../theme/theme';
+import HeaderBar from '../components/HeaderBar';
+import EmptyListAnimation from '../components/EmptyListAnimation';
 
 const CartScreen = () => {
-  const cartList = useStore((state: any) => state.CartList);
-  console.log("CartList = " + cartList.length);
+  const CartList = useStore((state: any) => state.CartList);
+  const CartPrice = useStore((state: any) => state.CartPrice);
+  const incrementCartItemQuantity = useStore(
+    (state: any) => state.incrementCartItemQuantity,
+  );
+  const decrementCartItemQuantity = useStore(
+    (state: any) => state.decrementCartItemQuantity,
+  );
+  const calculateCartPrice = useStore((state: any) => state.calculateCartPrice);
+  const tabBarHeight = useBottomTabBarHeight();
+
   return (
-    <View>
-      <Text>CartScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View
+          style={[styles.ScrollViewInnerView, {marginBottom: tabBarHeight}]}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Cart" />
+            {CartList.length === 0 ? (
+              <EmptyListAnimation title="Cart is Empty..." />
+            ) : (
+              <></>
+            )}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+});
 
 export default CartScreen;
