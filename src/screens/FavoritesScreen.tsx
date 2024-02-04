0448973b812fc5fc4af6b99fb19c529d99c37c9e
ashim1588 <ashimmehar158@gmail.com>
2024-02-04 22:33:50 +0530

    Favourites Screen setup

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -1,14 +1,82 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {COLORS, SPACING} from '../theme/theme';
+import EmptyListAnimation from '../components/EmptyListAnimation';
+import HeaderBar from '../components/HeaderBar';
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ({navigation}: any) => {
+  const FavouritesList = useStore((state: any) => state.FavoritesList);
+  const tabBarHeight = useBottomTabBarHeight();
+  const addToFavoriteList = useStore((state: any) => state.addToFavoriteList);
+  const deleteFromFavoriteList = useStore(
+    (state: any) => state.deleteFromFavoriteList,
+  );
+  const ToggleFavourite = (favourite: boolean, type: string, id: string) => {
+    favourite ? deleteFromFavoriteList(type, id) : addToFavoriteList(type, id);
+  };
   return (
-    <View>
-      <Text>FavoritesScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View
+          style={[styles.ScrollViewInnerView, {marginBottom: tabBarHeight}]}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Favourites List" />
+            {FavouritesList.length === 0 ? (
+              <EmptyListAnimation title="No Favourites..." />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {FavouritesList.map((data: any) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.push('Details', {
+                        index: data.index,
+                        id: data.id,
+                        type: data.type,
+                      });
+                    }}
+                    key={data.id}>
+                    <View />
+                  </TouchableOpacity>
+                ))}
+              </View>
+            )}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  },
+});
 
 export default FavoritesScreen;
